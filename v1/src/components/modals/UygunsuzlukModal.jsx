932a45ccc51e2ea43fc8125d4410dd5a526625ae
koraykoksal <kksl.koray@gmail.com>
2023-09-11 15:21:19 +0300

    uygunsuzluk modal v1

--- v1/src/components/modals/UygunsuzlukModal.jsx
+++ v1/src/components/modals/UygunsuzlukModal.jsx
@@ -3,13 +3,21 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Formik,Form } from 'formik';
+import { Container, TextField, TextareaAutosize } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { sorunTipi,aksiyonSahibi } from '../../helpers/ProcessData';
+import Textarea from '@mui/joy/Textarea';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 500,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -17,8 +25,7 @@ const style = {
 };
 
 const UygunsuzlukModal=({open,setOpen,handleOpen})=>{
-//   const [open, setOpen] = React.useState(false);
-//   const handleOpen = () => setOpen(true);
+
   const handleClose = () => setOpen(false);
 
   return (
@@ -32,12 +39,160 @@ const UygunsuzlukModal=({open,setOpen,handleOpen})=>{
         aria-describedby="keep-mounted-modal-description"
       >
         <Box sx={style}>
-          <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <Typography id="keep-mounted-modal-title" variant="h6" component="h2" color="#C70039">
+            Uygunsuzluk
           </Typography>
-          <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+
+        
+           <Formik>
+
+            <Form>
+            
+            <Box sx={{mt:3,display:'flex',flexWrap:'wrap',gap:2}}>
+
+            <div style={{display:'flex',gap:3}}>
+            <TextField
+            sx={{width:'200px'}}
+            name="date"
+            id="date"
+            type="date"
+            />
+
+            <FormControl sx={{width:'180px'}}>
+                <InputLabel id="vardiya">Vardiya</InputLabel>
+                <Select
+                labelId="vardiya"
+                id="vardiya"
+                name='vardiya'
+                label="vardiya"
+                >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                </Select>
+            </FormControl>
+            </div>
+
+            <FormControl fullWidth>
+                <InputLabel id="is_merkezi">Makine</InputLabel>
+                <Select
+                labelId="is_merkezi"
+                id="is_merkezi"
+                name='is_merkezi'
+                label="is_merkezi"
+                >
+                <MenuItem value={1}>SK-KP1</MenuItem>
+                <MenuItem value={2}>SK-KP2</MenuItem>
+                <MenuItem value={3}>SK-KP3</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+                <InputLabel id="renk_kodu">Renk Kodu</InputLabel>
+                <Select
+                labelId="renk_kodu"
+                id="renk_kodu"
+                name='renk_kodu'
+                label="renk_kodu"
+                >
+                <MenuItem value={1}>ASC</MenuItem>
+                <MenuItem value={2}>ASD</MenuItem>
+                <MenuItem value={3}>ASF</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+                <InputLabel id="urun_kodu">Ürün Kodu</InputLabel>
+                <Select
+                labelId="urun_kodu"
+                id="urun_kodu"
+                name='urun_kodu'
+                label="urun_kodu"
+                >
+                <MenuItem value={1}>GRM23DZ</MenuItem>
+                <MenuItem value={2}>BNC02CT</MenuItem>
+                <MenuItem value={3}>VNT22KS</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+                <InputLabel id="urun_kodu">Sorun Tipi</InputLabel>
+                <Select
+                labelId="sorun_tipi"
+                id="sorun_tipi"
+                name='sorun_tipi'
+                label="sorun_tipi"
+                >
+                {
+                    sorunTipi.map((item)=>(
+                        <MenuItem value={item.text}>{item.text}</MenuItem>
+                    ))
+                }
+                
+                </Select>
+            </FormControl>
+
+            <TextField
+            fullWidth
+            label="Uygunsuz Deger-İşlem"
+            name="uygunsuz_deger"
+            id="uygunsuz_deger"
+            type="text"
+            variant="outlined"
+            />
+
+            <TextField
+            fullWidth
+            label="Standart Değer Aralığı"
+            name="standart_deger"
+            id="standart_deger"
+            type="text"
+            variant="outlined"
+            />
+
+            <FormControl fullWidth>
+                <InputLabel id="aksiyon_sahibi">Aksiyon Sahibi Bölüm</InputLabel>
+                <Select
+                labelId="aksiyon_sahibi"
+                id="aksiyon_sahibi"
+                name='aksiyon_sahibi'
+                label="aksiyon_sahibi"
+                >
+                {
+                    aksiyonSahibi.map((item)=>(
+                        <MenuItem value={item.text}>{item.text}</MenuItem>
+                    ))
+                }
+                
+                </Select>
+            </FormControl>
+
+            <Textarea
+            sx={{width:'100vh'}}
+            placeholder='Açıklama'
+            minRows={3}
+            />
+
+            <Textarea
+            sx={{width:'100vh'}}
+            placeholder='Aksiyon'
+            minRows={3}
+            />
+
+            <Button
+            variant='contained'
+            fullWidth
+            >
+                Save
+            </Button>
+
+
+            </Box>
+
+            </Form>
+           </Formik>
+
+          
         </Box>
       </Modal>
     </div>
---
+++ v1/src/helpers/ProcessData.js
@@ -0,0 +1,73 @@
+
+
+export const sorunTipi=[
+    {
+        text:"Ayak Krığı"
+    },
+    {
+        text:"Ayak Silimi"
+    },
+    {
+        text:"Basınç Değeri Sınır Dışı"
+    },
+    {
+        text:"Biskuvi Silimi"
+    },
+    {
+        text:"Boya Lekesi"
+    },
+    {
+        text:"Çapak"
+    },
+    {
+        text:"Çökme"
+    },
+    {
+        text:"Ayak Krığı"
+    },
+    {
+        text:"Dijital Logo Uygulama Hatası"
+    },
+    {
+        text:"Eksen Farkı"
+    },
+    {
+        text:"Kazan Karışması"
+    },
+    {
+        text:"Kenar Rötuş"
+    },
+    {
+        text:"Silim Suyu"
+    },
+    {
+        text:"Sır Kalınlığı"
+    },
+    {
+        text:"Spot İzi"
+    },
+    {
+        text:"Ürün Kimlik Kartı Uygunsuzluğu"
+    },
+    {
+        text:"Yoğunluk Değeri"
+    },
+
+
+]
+
+
+export const aksiyonSahibi=[
+    {
+        text:"DEKOR"
+    },
+    {
+        text:"DİJİTAL LOGO"
+    },
+    {
+        text:"ŞEKİLLENDİRME"
+    },
+    {
+        text:"SIRLAMA"
+    },
+]
