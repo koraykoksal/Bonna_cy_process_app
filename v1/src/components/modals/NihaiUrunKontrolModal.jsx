7d20e7945f27694a9f2358e73cb4139c664b4b0f
koraykoksal <kksl.koray@gmail.com>
2023-09-13 16:41:36 +0300

    nihaiUrunkontrol page and modal done

---
+++ v1/src/components/modals/NihaiUrunKontrolModal.jsx
@@ -0,0 +1,423 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Formik,Form } from 'formik';
+import { Container, IconButton, TextField, TextareaAutosize } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { sorunTipi,aksiyonSahibi } from '../../helpers/ProcessData';
+import Textarea from '@mui/joy/Textarea';
+import CloseIcon from '@mui/icons-material/Close';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { useState ,useEffect} from 'react';
+import {uygunsuzlukTipi} from "../../helpers/ProcessData"
+import {useSelector} from "react-redux"
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 525,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+
+};
+
+const NihaiUrunKontrolModal=({open,setOpen,handleOpen})=>{
+
+  const handleClose = () => setOpen(false);
+
+  const handleChange=(e)=>{
+    setnihaiUrunData({...nihaiUrunData,[e.target.name]:e.target.value})
+  }
+
+  const nowData=new Date()
+  const currentdate = nowData.getDate() +"-"+(nowData.getMonth()+1)+"-"+nowData.getFullYear()
+  const currentTime = nowData.getHours() +":"+nowData.getMinutes()
+
+  const {currentUser} = useSelector((state) => state.auth)
+
+  let getVardiya = 0;
+
+  const getShift=()=>{
+    const now=new Date().getHours()
+
+    if(now > 8 && now < 16){
+        getVardiya = 2
+    }
+    else if(now > 16 && now < 23){
+        getVardiya = 3
+    }
+    else{
+        getVardiya = 1
+    }
+
+    return getVardiya
+
+  }
+
+  const [nihaiUrunData, setnihaiUrunData] = useState({
+    renkKodu:"",
+    aciklama:"",
+    urun_kodu:"",
+    olculenNumuneSayisi:"",
+    agirlik:"",
+    cap_ab:"",
+    cap_cd:"",
+    cap_e1e2:"",
+    yukseklik_a:"",
+    yukseklik_b:"",
+    yukseklik_c:"",
+    yukseklik_d:"",
+    icYukseklik:"",
+    dudak_a:"",
+    dudak_b:"",
+    dudak_c:"",
+    dudak_d:"",
+    ayakYuksekligi:"",
+    bombeCokme:"",
+    duzlemdenSapma:"",
+  
+    vardiya:getShift(),
+    date:currentdate.toString(),
+    time:currentTime.toString(),
+    kontroleden_kisi:currentUser
+  })
+
+
+  console.log(nihaiUrunData)
+
+
+  return (
+    <div>
+      <Button onClick={handleOpen} variant='outlined'>New</Button>
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={style}>
+
+        <Box sx={{display:'flex',flexWrap:'wrap',justifyContent:'space-between',alignItems:'center'}}>
+
+            <Typography id="keep-mounted-modal-title" variant="h6" component="h2" color="#000000">
+                Nihai Ürün Kontrol
+            </Typography>
+
+            <IconButton onClick={()=>handleClose()}>
+                <HighlightOffIcon sx={{color:'#C70039',fontSize:'28px'}}/>
+            </IconButton>
+        </Box>
+          
+            
+            <Box sx={{mt:3,display:'flex',flexDirection:'column',gap:2}} component='form'>
+                
+
+            <Box sx={{display:'flex',justifyContent:'center',gap:2}}>
+
+              {/* makine */}
+            <FormControl fullWidth>
+                <InputLabel id="renkKodu">Renk Kodu</InputLabel>
+                <Select
+                labelId="renkKodu"
+                id="renkKodu"
+                name='renkKodu'
+                label="renkKodu"
+                value={nihaiUrunData.renkKodu}
+                onChange={handleChange}
+                >
+                <MenuItem value="ASC">ASC</MenuItem>
+                <MenuItem value="ASD">ASD</MenuItem>
+                <MenuItem value="ASF">ASF</MenuItem>
+                </Select>
+            </FormControl>
+            
+            {/* ürün kodu */}
+            <FormControl fullWidth>
+                <InputLabel id="urun_kodu">Ürün Kodu</InputLabel>
+                <Select
+                labelId="urun_kodu"
+                id="urun_kodu"
+                name='urun_kodu'
+                label="urun_kodu"
+                value={nihaiUrunData.urun_kodu}
+                onChange={handleChange}
+                >
+                <MenuItem value="GRM23DZ">GRM23DZ</MenuItem>
+                <MenuItem value="BNC02CT">BNC02CT</MenuItem>
+                <MenuItem value="VNT22KS">VNT22KS</MenuItem>
+                </Select>
+            </FormControl>
+            
+            </Box>
+
+            <Box sx={{display:'flex',justifyContent:'center',gap:2}}>
+
+            
+            <TextField
+            fullWidth
+            label="Ölçülen Numune Sayısı"
+            name="olculenNumuneSayisi"
+            id="olculenNumuneSayisi"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.olculenNumuneSayisi}
+            onChange={handleChange}
+            />
+
+
+            <TextField
+            fullWidth
+            label="Ağırlık (gr)"
+            name="agirlik"
+            id="agirlik"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.agirlik}
+            onChange={handleChange}
+            />
+            </Box>
+
+            <Box>
+
+            <Typography variant='subtitle2' align='center'>
+              Çap (gr)
+            </Typography>
+            
+            <Box sx={{display:'flex',justifyContent:'center',gap:2}}>
+            <TextField
+            fullWidth
+            label="A-B Uzun Kenar"
+            name="cap_ab"
+            id="cap_ab"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.cap_ab}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="C-D"
+            name="cap_cd"
+            id="cap_cd"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.cap_cd}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="E1-E2"
+            name="cap_e1e2"
+            id="cap_e1e2"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.cap_e1e2}
+            onChange={handleChange}
+            />
+            </Box>
+            
+            </Box>
+
+            <Box>
+
+            <Typography variant='subtitle2' align='center'>
+              Yükseklik (mm)
+            </Typography>
+            
+            <Box sx={{display:'flex',justifyContent:'center',gap:2}}>
+            <TextField
+            fullWidth
+            label="A"
+            name="yukseklik_a"
+            id="yukseklik_a"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.yukseklik_a}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="B"
+            name="yukseklik_b"
+            id="yukseklik_b"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.yukseklik_b}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="C"
+            name="yukseklik_c"
+            id="yukseklik_c"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.yukseklik_c}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="D"
+            name="yukseklik_d"
+            id="yukseklik_d"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.yukseklik_d}
+            onChange={handleChange}
+            />
+            </Box>
+            
+            </Box>
+
+            <TextField
+            fullWidth
+            label="İç Yükseklik (mm)"
+            name="icYukseklik"
+            id="icYukseklik"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.icYukseklik}
+            onChange={handleChange}
+            />
+
+            <Box>
+
+            <Typography variant='subtitle2' align='center'>
+              Dudak (mm)
+            </Typography>
+            
+            <Box sx={{display:'flex',justifyContent:'center',gap:2}}>
+            <TextField
+            fullWidth
+            label="A"
+            name="dudak_a"
+            id="dudak_a"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.dudak_a}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="B"
+            name="dudak_b"
+            id="dudak_b"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.dudak_b}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="C"
+            name="dudak_c"
+            id="dudak_c"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.dudak_c}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="D"
+            name="dudak_d"
+            id="dudak_d"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.dudak_d}
+            onChange={handleChange}
+            />
+            </Box>
+            
+            </Box>
+
+            <Box sx={{display:'flex',justifyContent:'center',gap:2}}>
+
+            <TextField
+            fullWidth
+            label="Ayak Yüksekliği"
+            name="ayakYuksekligi"
+            id="ayakYuksekligi"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.ayakYuksekligi}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="Bombe Çökme"
+            name="bombeCokme"
+            id="bombeCokme"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.bombeCokme}
+            onChange={handleChange}
+            />
+
+            <TextField
+            fullWidth
+            label="Düzlemden Sapma"
+            name="duzlemdenSapma"
+            id="duzlemdenSapma"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.duzlemdenSapma}
+            onChange={handleChange}
+            />
+
+            </Box>
+
+            <TextField
+            multiline
+            fullWidth
+            label="Açıklama"
+            name="aciklama"
+            id="aciklama"
+            type="text"
+            variant="outlined"
+            value={nihaiUrunData.aciklama}
+            onChange={handleChange}
+            />
+
+           
+
+
+            <Button
+            variant='contained'
+            fullWidth
+            type='submit'
+            >
+                Save
+            </Button>
+
+
+            </Box>
+
+          
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default NihaiUrunKontrolModal
--- v1/src/pages/Nihaiurunkontrol.jsx
+++ v1/src/pages/Nihaiurunkontrol.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import {typoStyle} from "../styles/globalStyle"
 import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
+import NihaiUrunKontrolModal from '../components/modals/NihaiUrunKontrolModal';
 
 
 
 const Nihaiurunkontrol = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+
+
   return (
     
     <div>
         <Typography mt={8} align='center' variant='subtitle1' sx={typoStyle}>
             Nihai Ürün Kontrol
         </Typography>
+
+        <Box>
+          <NihaiUrunKontrolModal open={open} setOpen={setOpen} handleOpen={handleOpen}/>
+        </Box>
     </div>
 
   )
