91f0218caf6d871602c4b5423d7068d9e72cafac
koraykoksal <kksl.koray@gmail.com>
2023-12-15 07:45:34 +0300

    up

--- v2/src/components/modals/IzostatikPresModal.jsx
+++ v2/src/components/modals/IzostatikPresModal.jsx
@@ -24,7 +24,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 525,
+  width: 725,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
--- v2/src/index.js
+++ v2/src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import HttpsRedirect from "react-https-redirect"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  
 
-  <App />
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>
 
-    
   // </React.StrictMode>
 );
 
